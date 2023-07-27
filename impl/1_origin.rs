d825eaf247bf71d52a83995c3b77bca299094836
allaboutevemirolive <xkmxlfirdxus@gmail.com>
2023-07-27 18:41:28 -0400

    Initial Commit

--- impl/1_origin.rs
+++ impl/1_origin.rs
@@ -1,42 +1,42 @@
-    #[cfg(not(no_global_oom_handling))]
-    #[unstable(feature = "string_remove_matches", reason = "new API", issue = "72826")]
-    pub fn remove_matches<'a, P>(&'a mut self, pat: P)
-    where
-        P: for<'x> Pattern<'x>,
-    {
-        use core::str::pattern::Searcher;
+#[cfg(not(no_global_oom_handling))]
+#[unstable(feature = "string_remove_matches", reason = "new API", issue = "72826")]
+pub fn remove_matches<'a, P>(&'a mut self, pat: P)
+where
+    P: for<'x> Pattern<'x>,
+{
+    use core::str::pattern::Searcher;
 
-        let rejections = {
-            let mut searcher = pat.into_searcher(self);
+    let rejections = {
+        let mut searcher = pat.into_searcher(self);
 
-            let mut front = 0;
-            let rejections: Vec<_> = from_fn(|| {
-                let (start, end) = searcher.next_match()?;
-                let prev_front = front;
-                front = end;
-                Some((prev_front, start))
-            })
-            .collect();
-            rejections
-                .into_iter()
-                .chain(core::iter::once((front, self.len())))
-        };
+        let mut front = 0;
+        let rejections: Vec<_> = from_fn(|| {
+            let (start, end) = searcher.next_match()?;
+            let prev_front = front;
+            front = end;
+            Some((prev_front, start))
+        })
+        .collect();
+        rejections
+            .into_iter()
+            .chain(core::iter::once((front, self.len())))
+    };
 
-        let mut len = 0;
-        let ptr = self.vec.as_mut_ptr();
+    let mut len = 0;
+    let ptr = self.vec.as_mut_ptr();
 
-        for (start, end) in rejections {
-            let count = end - start;
-            if start != len {
+    for (start, end) in rejections {
+        let count = end - start;
+        if start != len {
                 
-                unsafe {
-                    ptr::copy(ptr.add(start), ptr.add(len), count);
-                }
+            unsafe {
+                ptr::copy(ptr.add(start), ptr.add(len), count);
             }
-            len += count;
         }
-
-        unsafe {
-            self.vec.set_len(len);
-        }
+        len += count;
     }
+
+    unsafe {
+        self.vec.set_len(len);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,215 @@
-#![feature(string_remove_matches)]
+// The iterator-based approach can be efficient
+// for small patterns or texts with few occurrences of the
+// pattern. However, if the pattern or text is large, or if
+// there are many occurrences of the pattern, it may not be
+// as efficient as other algorithms or regular expressions.
 
-fn main() { }
+pub struct RemovePatternIter<'a> {
+    text: &'a str,
+    pattern: &'a str,
+    finished: bool,
+}
+
+impl<'a> Iterator for RemovePatternIter<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.finished {
+            return None;
+        }
+
+        // Handle the case when the pattern is an empty string
+        if self.pattern.is_empty() {
+            self.finished = true;
+            return Some(self.text);
+        }
+
+        if let Some(pos) = self.text.find(self.pattern) {
+            let non_match = &self.text[..pos];
+            // Skip pattern and return remainder
+            self.text = &self.text[pos + self.pattern.len()..];
+            Some(non_match)
+        } else {
+            self.finished = true;
+            Some(self.text)
+        }
+    }
+}
+
+pub trait RemovePattern<'a>: Sized {
+    fn remove_pattern(self, pattern: &'a str) -> RemovePatternIter<'a>;
+}
+
+impl<'a> RemovePattern<'a> for &'a str {
+    fn remove_pattern(self, pattern: &'a str) -> RemovePatternIter<'a> {
+        RemovePatternIter {
+            text: self,
+            pattern,
+            finished: false,
+        }
+    }
+}
+
+// Example usage
+fn main() {
+    let text = "Hello, **Rust**! How are you doing **today**?";
+    let pattern = "**";
+
+    let result: String = text.remove_pattern(pattern).collect(); // Collect the iterator into a new String
+    println!("{}", result); // Output: "Hello, Rust! How are you doing today?"
+}
+
+// Test cases
+#[test]
+fn test_no_pattern_occurrence() {
+    let text = "No pattern here.";
+    let pattern = "**";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "No pattern here.");
+}
 
 #[test]
-fn test_remove_matches() {
+fn test_single_pattern_occurrence() {
+    let text = "Pattern ** found.";
+    let pattern = "**";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "Pattern  found.");
+}
 
-    // test_single_pattern_occurrence
-    let mut s = "abc".to_string();
-    s.remove_matches('b');
-    assert_eq!(s, "ac");
-    // repeat_test_single_pattern_occurrence
-    s.remove_matches('b');
-    assert_eq!(s, "ac");
+#[test]
+fn test_multiple_pattern_occurrences() {
+    let text = "Pattern ** found ** twice.";
+    let pattern = "**";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "Pattern  found  twice.");
+}
 
-    // test_single_character_pattern
-    let mut s = "abcb".to_string();
-    s.remove_matches('b');
-    assert_eq!(s, "ac");
+#[test]
+fn test_pattern_at_beginning() {
+    let text = "**Pattern at the beginning.";
+    let pattern = "**";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "Pattern at the beginning.");
+}
 
-    // test_pattern_with_special_characters
-    let mut s = "ศไทย中华Việt Nam; foobarศ".to_string();
-    s.remove_matches('ศ');
-    assert_eq!(s, "ไทย中华Việt Nam; foobar");
+#[test]
+fn test_pattern_at_end() {
+    let text = "Pattern at the end.**";
+    let pattern = "**";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "Pattern at the end.");
+}
 
-    // test_pattern_empty_text_and_pattern
-    let mut s = "".to_string();
-    s.remove_matches("");
-    assert_eq!(s, "");
+#[test]
+fn test_empty_text() {
+    let text = "";
+    let pattern = "**";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "");
+}
 
-    // test_pattern_empty_text
-    let mut s = "".to_string();
-    s.remove_matches("something");
-    assert_eq!(s, "");
+// TODO: stuck
+#[test]
+fn test_empty_pattern() {
+    let text = "Testing with empty pattern.";
+    let pattern = "";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, text);
+}
 
-    // test_empty_pattern
-    let mut s = "Testing with empty pattern.".to_string();
-    s.remove_matches("");
-    assert_eq!(s, "Testing with empty pattern.");
+#[test]
+fn test_large_text_and_pattern() {
+    let text = "Large text with multiple pattern occurrences: **Pattern** **Pattern** **Pattern**.";
+    let pattern = "**Pattern**";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "Large text with multiple pattern occurrences:   .");
+}
 
-    // test_multiple_consecutive_patterns_1
-    let mut s = "aaaaa".to_string();
-    s.remove_matches('a');
-    assert_eq!(s, "");
+#[test]
+fn test_pattern_not_found() {
+    let text = "No pattern in this text.";
+    let pattern = "missing";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, text);
+}
 
-    // test_multiple_consecutive_patterns_2
-    let mut s = "Hello **world****today!**".to_string();
-    s.remove_matches("**");
-    assert_eq!(s, "Hello worldtoday!");
+#[test]
+fn test_pattern_empty_text() {
+    let text = "";
+    let pattern = "something";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "");
+}
 
-    // test_case_insensitive_pattern
-    let mut s = "CASE ** SeNsItIvE ** PaTtErN.".to_string();
-    s.remove_matches("sEnSiTiVe");
-    assert_eq!(s, "CASE ** SeNsItIvE ** PaTtErN.");
+#[test]
+fn test_pattern_empty_text_and_pattern() {
+    let text = "";
+    let pattern = "";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "");
+}
 
-    // test_pattern_with_digits
-    let mut s = "123 ** 456 ** 789".to_string();
-    s.remove_matches("**");
-    assert_eq!(s, "123  456  789");
+#[test]
+fn test_single_character_pattern() {
+    let text = "a b c d e";
+    let pattern = " ";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "abcde");
+}
 
-    // test_pattern_occurs_after_empty_string
-    let mut s = "abc X defXghi".to_string();
-    s.remove_matches("X");
-    assert_eq!(s, "abc  defghi");
+#[test]
+fn test_large_pattern() {
+    let text = "aaaXbbbXcccXdddXeee";
+    let pattern = "X";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "aaabbbcccdddeee");
+}
 
-    // test_large_pattern
-    let mut s = "aaaXbbbXcccXdddXeee".to_string();
-    s.remove_matches("X");
-    assert_eq!(s, "aaabbbcccdddeee");
+#[test]
+fn test_pattern_occurs_after_empty_string() {
+    let text = "abc X defXghi";
+    let pattern = "X";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "abc  defghi");
+}
 
-    // test_pattern_at_multiple_positions
-    let mut s = "Pattern ** found ** multiple ** times ** in ** text.".to_string();
-    s.remove_matches("**");
-    assert_eq!(s, "Pattern  found  multiple  times  in  text.");
+#[test]
+fn test_multiple_consecutive_patterns() {
+    let text = "Hello **world****today!**";
+    let pattern = "**";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "Hello worldtoday!");
+}
 
+#[test]
+fn test_pattern_with_special_characters() {
+    let text = "Testing with $$$ pattern $$$";
+    let pattern = "$$$";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "Testing with  pattern ");
+}
+
+#[test]
+fn test_pattern_at_multiple_positions() {
+    let text = "Pattern ** found ** multiple ** times ** in ** text.";
+    let pattern = "**";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "Pattern  found  multiple  times  in  text.");
+}
+
+#[test]
+fn test_pattern_with_digits() {
+    let text = "123 ** 456 ** 789";
+    let pattern = "**";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "123  456  789");
+}
+
+// TODO: enable case-sensitive?
+#[test]
+fn test_case_insensitive_pattern() {
+    let text = "CASE ** SeNsItIvE ** PaTtErN.";
+    let pattern = "sEnSiTiVe";
+    let result: String = text.remove_pattern(pattern).collect();
+    assert_eq!(result, "CASE ** SeNsItIvE ** PaTtErN.");
 }
